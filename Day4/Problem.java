84149948fedb4aab70cb03659aac0b522d640a06
sathi7029 <sathiruidas7029@gmail.com>
2023-03-01 22:20:53 +0530

    day4 uploaded

---
+++ Day4/Problem.java
@@ -0,0 +1,17 @@
+package Day4;
+public class Problem{
+    static int element_sum(int a[]){
+        int n=a.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+                sum+=a[i];
+        }
+        return sum;
+
+    }
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5};
+        int sum=element_sum(a);
+        System.out.println("sum is:"+sum);
+    }
+}
