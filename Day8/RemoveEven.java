c4ceb9820be4218e2a1929ef6294ef8e8e7363ff
sathi7029 <sathiruidas7029@gmail.com>
2023-03-05 20:10:10 +0530

    day8 uploaded

---
+++ Day8/RemoveEven.java
@@ -0,0 +1,30 @@
+package Day8;
+
+import java.util.ArrayList;
+
+public class RemoveEven {
+    public static void removeEven(ArrayList<Integer>a){
+        
+        for(int i=a.size()-1;i>=0;i--){
+            if(a.get(i)%2==0){
+                a.remove(i);
+            }
+        }
+        
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer>al=new ArrayList<>();
+        al.add(5);
+        al.add(10);
+        al.add(4);
+        al.add(15);
+        al.add(8);
+        al.add(21);
+        al.add(3);
+        al.add(2);
+        System.out.println(al);
+        System.out.println();
+        removeEven(al);
+        System.out.println(al);
+    }
+}
